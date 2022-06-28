de3a43a7254fcc77fe0f5830ee9df0924f673bd6
BogusD <qazxwd44@hanmail.net>
2022-06-28 22:40:09 +0900

    updateposco

--- posco/script2.js
+++ posco/script2.js
@@ -1,6 +1,6 @@
-function display( name ){
+function display(name){
 	var list = document.getElementById(name);
-	if(list.style.display='none'){
+	if(list.style.display=='none'){
 		list.style.display = 'block';
 	}else{
 		list.style.display = 'none';

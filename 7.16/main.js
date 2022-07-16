b69319befcd4f3bb56393c8977a8116a77358185
천해성 <100825999+BoGusD@users.noreply.github.com>
2022-07-16 15:14:32 +0900

    Update main.js

--- 7.16/main.js
+++ 7.16/main.js
@@ -15,8 +15,9 @@ function onSubmit(e) {
         setTimeout(()=>msg.style.display='none', 1000)
         return;
     }
-    else if(todos.childElementCount===8){
-    todos.classList.remove("item");
+    //todolist가 8개인 상황에서 하나 li를 제거해서 8줄 유지
+    if(todos.childElementCount===8){
+    todos.classList.remove("todos:nth-child(1)");
     }
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(todoInput.value));

d068f3014261eb52e58f16d401ffc0fe95e90da7
천해성 <100825999+BoGusD@users.noreply.github.com>
2022-07-16 15:04:40 +0900

    Update main.js

--- 7.16/main.js
+++ 7.16/main.js
@@ -15,18 +15,16 @@ function onSubmit(e) {
         setTimeout(()=>msg.style.display='none', 1000)
         return;
     }
-
+    else if(todos.childElementCount===8){
+    todos.classList.remove("item");
+    }
     const li = document.createElement('li');
     li.appendChild(document.createTextNode(todoInput.value));
     li.classList.add("item");
     todos.appendChild(li);
     todoInput.value = '';
-    //8개이상일 때 맨 윗줄 삭제
-    const di = document.removeChild("todo-list");
-    di.appendChild(document.removeChild(todoInput.value));
-    if(di.childElementCount===8){
-        di.classList.remove("item");
 
-    }
+
+
 };
 

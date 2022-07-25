eb1b9bce34e8308fb7aa1568b72d91c230ee9e65
천해성 <100825999+BoGusD@users.noreply.github.com>
2022-07-26 04:47:54 +0900

    Update main.js

---
+++ 07.25/js/main.js
@@ -0,0 +1,11 @@
+const subMenu = document.querySelector('#snb .sub_menu');
+const snbBtn = document.querySelector("#snb .btn_menu");
+
+snbBtn.addEventListener("click", function() {
+  if(subMenu.classList.contains("hide")){
+    subMenu.classList.remove("hide");
+  }else {
+   subMenu.classList.add("hide");
+  }
+    
+})
